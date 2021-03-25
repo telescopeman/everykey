c6a1934499e25d32bc62a3192a57ce1e1647316f
Caleb Copeland <calebcopeland1@gmail.com>
2021-03-25 00:11:08 -0400

    fixed bug with filter creator display

--- Filter.java
+++ Filter.java
@@ -109,6 +109,8 @@ public class Filter
         }
 
     }
+    
+
 
     public String toString()
     {
--- FilterCreator.java
+++ FilterCreator.java
@@ -14,7 +14,7 @@ import java.util.Arrays;
  * @author (your name)
  * @version (a version number or a date)
  */
-public class FilterCreator extends JFrame implements ActionListener
+public class FilterCreator extends EasyFrame implements ActionListener
 {
     // instance variables - replace the example below with your own
     String type;
@@ -46,6 +46,7 @@ public class FilterCreator extends JFrame implements ActionListener
     private void setUpUniqueFactors(String id)
     {
         //removeAll();
+        clear();
 
         JLabel title = new JLabel("Filter Designer - " + id, JLabel.CENTER);
         title.setFont(new Font(title.getFont().getFontName(),Font.BOLD,12));
