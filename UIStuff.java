cedaeac910535983fba63a1b5df14720357e1eeb
Caleb <calebcopeland1@gmail.com>
2021-04-06 12:10:02 -0400

    misc

--- UIStuff.java
+++ UIStuff.java
@@ -2,6 +2,7 @@ import javax.swing.JMenu;
 import javax.swing.JScrollPane;
 import javax.swing.JMenuItem;
 import javax.swing.JMenuBar;
+
 import javax.swing.BoxLayout;
 import java.awt.BorderLayout; 
 
